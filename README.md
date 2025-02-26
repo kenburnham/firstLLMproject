22b18f34fa01b4a180058af9d284f4526c812f81
Ken Burnham <kenburnham@verizon.net>
2025-02-25 23:31:00 -0500

    Added sentiment analysis to llm.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # firstLLMproject
 This is my first attempt at a LLM project, while making pushes to github. Hopefully not the last! Will update the readme as the project begins to take form...
+
+Ensure you are remoted into code server
+Ensure you are using the llm-dev conda environment
+Ensure you are using the llm-dev conda python interpreter
--- llm.py
+++ llm.py
@@ -1,1 +1,12 @@
-llm.py
+from transformers import pipeline
+import torch
+
+classifier = pipeline('sentiment-analysis', device=0)  # Use GPU 0
+
+user_input = input("Give me a sentiment to check: ")
+while user_input != "stop":
+    result = classifier(user_input)
+    print(result)
+    user_input = input("Provide another sentiment! Or enter \"stop\": ")
+
+
